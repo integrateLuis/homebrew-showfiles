3a474853d970af72948713461f4f860b058ccd97
Luis Arellano <integrate.luis@gmail.com>
2025-01-23 10:16:28 -0600

    feat: update the SHA256 hash in the showfiles formula

--- Formula/showfiles.rb
+++ Formula/showfiles.rb
@@ -2,7 +2,7 @@ class Showfiles < Formula
     desc "Show the content of directories and files with syntax highlighting"
     homepage "https://github.com/integrateLuis/homebrew-showfiles"
     url "https://github.com/integrateLuis/homebrew-showfiles/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "PON_EL_SHA256_CORRECTO_AQUI"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     license "MIT"
   
     def install
